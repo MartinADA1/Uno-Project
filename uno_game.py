c8aa88c0a7b1db73fc9b05c594dcdc9b85d848e6
Martin Nkama <martin.nkama@ada.ac.uk>
2023-08-02 10:20:33 +0100

    multiple functions created for player v ai mode

--- uno_game.py
+++ uno_game.py
@@ -4,6 +4,40 @@ from hand import Hand
 from deck import Deck
 from termcolor import colored
 
+#Funciton to randomly select who starts first (Player Vs. AI)
+def choose_first_playerVAI():
+    if random.randint(0,1)==0:
+        return 'Player'
+    else:
+        return 'Pc'
+
+#Function to check if the card thrown by Player/AI is a valid card by comparing it with the top card
+def single_card_check(top_card,card):
+    if card.color==top_card.color or top_card.rank==card.rank or card.cardtype=='action_nocolor':
+        return True
+    else:
+        return False
+    
+#FOR AI ONLY
+#To check if AI has any valid card to throw 
+def full_hand_check(hand,top_card):
+    for c in hand.cards:
+        if c.color==top_card.color or c.rank == top_card.rank or c.cardtype=='action_nocolor':
+            return hand.remove_card(hand.cardsstr.index(str(c))+1)
+    else:
+        return 'no card'
+
+
+#Function to check if either wins
+def win_check(hand):
+    if len(hand.cards)==0:
+        return True
+    else:
+        return False
+    
+def playerVAI():
+
+#Main Menu
 while True:
 
     print(15*"-" + " Martin's UNO Main Menu " + 15*"-" + "\n\n")
